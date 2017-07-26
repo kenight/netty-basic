c3cb4baee5278462d82ff38e96ea50e1cf6215c0
kenight <ikenight@gamil.com>
2017-07-26 23:56:21 +0800

    update readme

--- src/main/java/example/codec/string/ServerChannelInitializer.java
+++ src/main/java/example/codec/string/ServerChannelInitializer.java
@@ -9,6 +9,14 @@ import io.netty.handler.codec.string.StringEncoder;
 
 public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> {
 
+	// ChannelPipeline 的执行顺序, 举例说明(官方文档)
+	// p.addLast("1", new InboundHandlerA());
+	// p.addLast("2", new InboundHandlerB());
+	// p.addLast("3", new OutboundHandlerA());
+	// p.addLast("4", new OutboundHandlerB());
+	// p.addLast("5", new InboundOutboundHandlerX());
+	// 如果是接收，执行顺序：1,2,5 (实现 ChannelInboundHandler 的处理类）
+	// 如果是流出，执行顺序：5,4,3 (实现 ChannelOutboundHandler 的处理类)
 	@Override
 	protected void initChannel(SocketChannel ch) throws Exception {
 		// 以("\n")为结尾分割的解码器
